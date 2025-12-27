e4bdcf3918df9918b7d6503f3ac8befb837a9167
Nathanael <nathanaelhacker6@gmail.com>
2025-12-27 08:48:38 +0100

    origin

--- app/main.py
+++ app/main.py
@@ -145,8 +145,8 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 api_router = APIRouter()
 
 api_router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+api_router.include_router(users.router, prefix="/users", tags=["Users"]) # 👈 **CORRECTION : Placer avant 'admin'**
 api_router.include_router(admin.router, prefix="/admin", tags=["Administration"])
-api_router.include_router(users.router, prefix="/users", tags=["Users"])
 api_router.include_router(students.router, prefix="/students", tags=["Students"])
 api_router.include_router(faculties.router, prefix="/faculties", tags=["Faculties & Structure"])
 api_router.include_router(courses.router, prefix="/courses", tags=["Courses (UE)"])
